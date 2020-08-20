a08fb9dd6e8327546f8f3301cf5b1469c2f2e5e4
Daniel Lemes <inventarartcomunicacao@hotmail.com>
2020-08-20 11:52:15 -0300

    alteraçoes

--- src/App.js
+++ src/App.js
@@ -11,76 +11,64 @@ import Filters from "./components/Filters";
 // border-right-width:thin;
 // `
 class App extends React.Component {
-  state = [
-    {
-    id:1,
-    name: "Item1",
-    value:"",
-    imageUrl: "",
-  },
-   {
-      id: 2,
-      name: "Item2",
-      value: "",
-      imageUrl: "",
-    },
-     {
-      id: 3,
-      name: "Item3",
-      value: "",
-      imageUrl: "",
-    },
-     {
-    id:4,
-    name: "Item4",
-    value:"",
-    imageUrl: "",
-  },
-   {
-    id:5,
-    name: "Item5",
-    value:"",
-    imageUrl: "",
-  },
-   {
-    id:6,
-    name: "Item6",
-    value:"",
-    imageUrl: "",
-  },
-   {
-    id:7,
-    name: "Item7",
-    value:"",
-    imageUrl: "",
-  },
-   {
-      id: 8,
-      name: "Item8",
-      value: "",
-      imageUrl: "",
-    }
-]
-  // renderizaEtapa = () => {
-  //   switch (this.state.id) {
-  //     case 1:
-  //       return  ;
-  //     case 2:
-  //       return <Etapa2 />;
-  //     case 3:
-  //       return <Etapa3 />;
-  //     case 4:
-  //       return <Final />;
-  //     default:
-  //       return <Final />;
+  // state = [
+  //   {
+  //     id: 1,
+  //     name: "Item1",
+  //     value: "",
+  //     imageUrl: "",
+  //   },
+  //   {
+  //     id: 2,
+  //     name: "Item2",
+  //     value: "",
+  //     imageUrl: "",
+  //   },
+  //   {
+  //     id: 3,
+  //     name: "Item3",
+  //     value: "",
+  //     imageUrl: "",
+  //   },
+  //   {
+  //     id: 4,
+  //     name: "Item4",
+  //     value: "",
+  //     imageUrl: "",
+  //   },
+  //   {
+  //     id: 5,
+  //     name: "Item5",
+  //     value: "",
+  //     imageUrl: "",
+  //   },
+  //   {
+  //     id: 6,
+  //     name: "Item6",
+  //     value: "",
+  //     imageUrl: "",
+  //   },
+  //   {
+  //     id: 7,
+  //     name: "Item7",
+  //     value: "",
+  //     imageUrl: "",
+  //   },
+  //   {
+  //     id: 8,
+  //     name: "Item8",
+  //     value: "",
+  //     imageUrl: "",
   //   }
-  // };
+  // ]
+  
+ 
   render(){
     
      return (
     <div >
         <Filters />   
-        <h1>itens do carrinho</h1>     
+        <h1></h1>     
     </div>
   );
   }
